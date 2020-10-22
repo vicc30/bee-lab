fde31afe91a344404a67c9af994c52100552eb44
Victor Cruz <vicc23@gmail.com>
2020-10-22 12:42:03 -0500

    Added SplashScreen component

---
+++ components/SplashScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+const logoSource = './images/bee.png';
+const frenchPink = '#FF5D8F'
+
+const SplashScreen = () => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: frenchPink
+            }}>
+            <Image
+                source={require(logoSource)}
+                style={{ width: 150, height: 150 }}
+            />
+        </View>
+    );
+}
+
+export default SplashScreen;
